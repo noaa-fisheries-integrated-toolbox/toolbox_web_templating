c8f26b68924f978723fa275f583f0abdde427ad2
corinne.bassin <corinne.bassin@noaa.gov>
2019-04-02 15:03:17 -0700

    add file looping and move and copy files

--- create_html_from_json.py
+++ create_html_from_json.py
@@ -2,14 +2,18 @@ from jinja2 import Environment, FileSystemLoader
 import argparse
 import json
 import os
+import shutil
 #basic templating
 
 
 
 
-def write_html_index(template, configs):
-    root = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(root, 'html', 'index.html')
+def write_html_index(template, configs, new_dir):
+    #root = os.path.dirname(os.path.abspath(__file__))
+    docsdir = os.path.join(new_dir, 'docs')
+    filename = os.path.join(new_dir, 'docs', 'index.html')
+    if not os.path.exists(docsdir):
+        os.mkdir(docsdir)
     with open(filename, 'w') as fh:
         fh.write(template.render(**configs))
 
@@ -40,16 +44,36 @@ def parse_args():
 
 
 
+def run_all_files():
+    # list_of_models =['2BOX','AGEPRO','AIM','ASAP','ASPIC','ATL',
+    #         'CSA','DCAC','IRATE','KALMAN','MCOMP','MSE','POPSIM-A',
+    #         'POPSIM-L','POPSIM','PSA','RIVARD','SCALE','SEINE','SRFIT',
+    #         'SS','STATCAM','VPA','VRD','YPR','YPRLEN']
+    list_of_models = ['2BOX']
+    for modelname in list_of_models:
+        old_name = modelname +'.json'
+        # shutil.copy(old_name, 'config_template.json')
+        new_dir = '../../legacy_models/' + modelname
+        # Create target Directory if don't exist
+        if not os.path.exists(new_dir):
+            os.mkdir(new_dir)
+        write_templates(old_name, new_dir)
+
+
+def write_templates(filename, new_dir):
+    # filename = parse_args()
+    configs = load_file_parse(filename)
+    template = load_template()
+    write_html_index(template, configs, new_dir)
+    staticdir = os.path.join(new_dir,'docs','static')
+    shutil.copy('./templates/static', staticdir)
+    shutil.copy('./templates/README.MD',staticdir)
+    shutil.copy('./templates/LICENSE.MD',staticdir)
 
 
 
 def main():
-    filename = parse_args()
-    configs = load_file_parse(filename)
-    template = load_template()
-    write_html_index(template, configs)
-
-
+    run_all_files()
 
 
 
