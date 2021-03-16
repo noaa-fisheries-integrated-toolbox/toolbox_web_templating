2c03aa843aae6537ed1a8bb8a4262c61031ab2a0
corinne.bassin <corinne.bassin@noaa.gov>
2021-03-16 09:45:46 -0600

    add model list clean

---
+++ model_list_dir/FaF_model_list/makemodeltojson/makemodeltojson.py
@@ -0,0 +1,61 @@
+#conda activate simple
+import pandas as pd
+import json
+
+
+
+
+def into_pandas(f):
+    df = pd.read_csv(f);
+    headers = [
+      "model_name",
+      "model_abbrev",
+      "model_type",
+      "author_name",
+      "background_text",
+       "references",
+      "online_app_link",
+      "download_app_link",
+      "github_link",
+      "version_number",
+      "pdf_download",
+      "badge_level",
+      "suport_level",
+      "documentation_link",
+      "associated_tools"
+    ]
+
+
+    new_df= pd.DataFrame(columns = headers)
+    new_df.model_name = df.Model
+    new_df.model_abbrev = df['Unnamed: 1']
+    new_df.version_number = df.Version
+    new_df.author_name = df.Author
+    for index, row in new_df.iterrows():
+        print(row)
+        if row.model_abbrev:
+            write_to_json(row)
+
+
+
+def write_to_json(x):
+
+    fn = x.model_abbrev.strip('()') + ".json"
+    with open(fn, 'w') as f:
+        json.dump(x.to_json(), f, ensure_ascii=False)
+
+
+
+
+
+
+def main():
+    filename = 'toolbox_survey_tally.csv'
+    into_pandas(f)
+
+
+
+
+if __name__ == '__main__':
+    main()
+
