3ef7ab67aca3c70353afbaf1b28d712ca6b247b3
k-doering-noaa <kathryn.doering@noaa.gov>
2022-12-06 10:37:33 -0500

    add way to generate html keyword snippet, add to template

--- json_analysis/parsejson.R
+++ json_analysis/parsejson.R
@@ -14,4 +14,22 @@ get_keywords <- function() {
     col.names = FALSE)
 }
 
+get_html_snippet <- function() {
+    files <- list.files(path = "model_list_dir", full.names = TRUE)
+    files <- grep("\\.json$", files, value = TRUE)
+
+    jsoninfo <- lapply(files, function(x) jsonlite::fromJSON(x))
+    # extract just the keywords
+    keywords <- lapply(jsoninfo, function(x) x[["keywords"]])
+    keywords <- unlist(keywords)
+    # rm dups from the keywords and alphabetize
+    keywords <- sort(unique(keywords))
+    # create the html snippits
+    snippet <- paste0('<input type="button" value="', keywords, '" onclick="simpleSearch(this.value)">')
+    write.table(snippet, file = file.path("json_analysis", "keywords_html_snippet.txt"), 
+    row.names = FALSE, col.names = FALSE, quote = FALSE)
+}
+
 get_keywords()
+
+get_html_snippet()
