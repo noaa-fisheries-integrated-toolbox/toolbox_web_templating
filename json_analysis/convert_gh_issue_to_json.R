f3d39bf40a0b58e45dac9f2c9f36f44ab1d91779
k-doering-noaa <kathryn.doering@noaa.gov>
2022-12-28 17:36:00 -0500

    Finish first draft of issue to json script

--- json_analysis/convert_gh_issue_to_json.R
+++ json_analysis/convert_gh_issue_to_json.R
@@ -1,6 +1,10 @@
-# different approach, convert info from a Github issue into json.
+# Convert info from a Github issue into json. This is
+# an important step toward making the toolbox 
+# onboarding automated.
 
+# Note that this is currently coded as an example for one repo only.
 library(gh)
+library(jsonlite)
 
 issue_info <- gh("/repos/{owner}/{repo}/issues/{issue_number}",
 owner = "noaa-fisheries-integrated-toolbox", 
@@ -29,6 +33,8 @@ issue_body <- issue_body[lengths(issue_body) != 0]
 issue_body <- lapply(issue_body, function(x) {
     json_title <- tolower(x[1])
     json_title <- gsub(" ", "_",  json_title, fixed = TRUE)
+    json_title <- gsub("(", "",  json_title, fixed = TRUE)
+    json_title <- gsub(")", "",  json_title, fixed = TRUE)
     x[1] <- json_title
     x
 })
@@ -45,3 +51,94 @@ issue_body$code_of_conduct <- NULL
 # Now make changes needed for specific elements to be correct in JSON.
 # Then we may need some custom work to convert the arrays.
 
+# Format boolean inputs
+issue_body$active_development <-
+  ifelse(issue_body$active_development == "Yes","true", "false")
+issue_body$noaa_internal <- 
+  ifelse(issue_body$noaa_internal == "Yes","true", "false")
+issue_body$uses_github_releases <- 
+  ifelse(issue_body$uses_github_releases == "Yes", "true", "false")
+
+# format toolbox drawers and keywords
+format_checkboxes <- function(original_checkboxes) {
+    tmp_boxes <- strsplit(original_checkboxes, "\n", fixed = TRUE)[[1]]
+    tmp_boxes <- grep("[X]", tmp_boxes, fixed = TRUE, value = TRUE)
+    tmp_boxes <- strsplit(tmp_boxes, "- [X] ", fixed = TRUE)
+    tmp_boxes <- unlist(lapply(tmp_boxes, function(x) x[2]))
+    tmp_boxes
+}
+
+issue_body$toolbox_drawers <- 
+  format_checkboxes(issue_body$toolbox_drawers)
+issue_body$keywords <- 
+  format_checkboxes(issue_body$keywords)
+
+# format other array inputs
+
+
+if(!is.null(issue_body$references)) {
+    tmp_refs <- issue_body$references
+    tmp_refs <- strsplit(tmp_refs, "(\\r)|(\\n)")[[1]]
+    issue_body$references <- tmp_refs
+}
+
+if(!is.null(issue_body$associated_tools)) {
+    tmp_tools <- issue_body$associated_tools
+    tmp_tools <- strsplit(tmp_tools, ",", fixed = TRUE)[[1]]
+    tmp_tools <- gsub("(\n)|(\r)", "", tmp_tools)
+    tmp_tools <- strsplit(tmp_tools, " http", fixed = TRUE)
+    tmp_tools <- lapply(tmp_tools, function(x) {
+          x[2] <- paste0("http", x[2])
+          names(x) <- c("name", "link")
+          x <- as.list(x)
+          x
+       })
+    issue_body$associated_tools <- tmp_tools
+}
+
+if(!is.null(issue_body$user_organizations)) {
+    tmp_orgs <- issue_body$user_organizations
+    tmp_orgs <- strsplit(tmp_orgs, ",\\s*")[[1]]
+    issue_body$user_organizations <- tmp_orgs
+}
+
+if(!is.null(issue_body$software_badges)) {
+    tmp_badges <- issue_body$software_badges
+    tmp_badges <- strsplit(tmp_badges, ",\\s*")[[1]]
+    issue_body$software_badges <- tmp_badges
+}
+
+# mark singletons before converting to JSON
+issue_body$active_development <- unbox(issue_body$active_development)
+issue_body$tool_name <- unbox(issue_body$tool_name)
+issue_body$tool_abbreviation <- unbox(issue_body$tool_abbreviation)
+issue_body$authors <- unbox(issue_body$authors)
+issue_body$noaa_internal <- unbox(issue_body$noaa_internal)
+issue_body$maintainer_name <- unbox(issue_body$maintainer_name)
+issue_body$maintainer_email <- unbox(issue_body$maintainer_email)
+issue_body$background_text <- unbox(issue_body$background_text)
+issue_body$citation <- unbox(issue_body$citation)
+issue_body$online_app_link <- unbox(issue_body$online_app_link)
+issue_body$executable_link <- unbox(issue_body$executable_link)
+issue_body$website_link <- unbox(issue_body$website_link)
+issue_body$documentation_link <- unbox(issue_body$documentation_link)
+issue_body$source_code_link <- unbox(issue_body$source_code_link)
+issue_body$pdf_download_link <- unbox(issue_body$pdf_download_link)
+issue_body$uses_github_releases <- unbox(issue_body$uses_github_releases)
+issue_body$static_version_number <- unbox(issue_body$static_version_number)
+issue_body$release_badge <- unbox(issue_body$release_badge)
+
+# to write to a file:
+#jsonlite::write_json(issue_body, "test.JSON", pretty = TRUE)
+
+markdown_txt <- paste("```",
+jsonlite::toJSON(issue_body, pretty = TRUE), 
+"```", sep = "\n"
+)
+
+# post json as a comment on the issue
+gh("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", 
+   owner = "noaa-fisheries-integrated-toolbox",
+   repo = "onboard-and-update", 
+   issue_number = 1, 
+   body = markdown_txt)
